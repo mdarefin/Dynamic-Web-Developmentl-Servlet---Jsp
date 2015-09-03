6e0589acd5d5fd95b9f30857e7266c9aef1d5f12
Md Arefin <arefin.shahrear@gmail.com>
2015-09-03 08:50:12 -0400

    adding GUI review

--- CST4713/src/review2/GUI/Hwk2JFrame.java
+++ CST4713/src/review2/GUI/Hwk2JFrame.java
@@ -27,10 +27,23 @@ public class Hwk2JFrame extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        colorGroup = new javax.swing.ButtonGroup();
         jLabel1 = new javax.swing.JLabel();
         jLabel2 = new javax.swing.JLabel();
         nameTextField = new javax.swing.JTextField();
         jLabel3 = new javax.swing.JLabel();
+        redRB = new javax.swing.JRadioButton();
+        greenRB = new javax.swing.JRadioButton();
+        blueRB = new javax.swing.JRadioButton();
+        jLabel4 = new javax.swing.JLabel();
+        stateList = new javax.swing.JComboBox();
+        jLabel5 = new javax.swing.JLabel();
+        securityCB = new javax.swing.JCheckBox();
+        appCB = new javax.swing.JCheckBox();
+        roboticCB = new javax.swing.JCheckBox();
+        clearB = new javax.swing.JButton();
+        submitB = new javax.swing.JButton();
+        exitB = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -46,35 +59,126 @@ public class Hwk2JFrame extends javax.swing.JFrame {
 
         jLabel3.setText("Preferred Color:");
 
+        colorGroup.add(redRB);
+        redRB.setText("Red");
+
+        colorGroup.add(greenRB);
+        greenRB.setText("Green");
+
+        colorGroup.add(blueRB);
+        blueRB.setText("Blue");
+
+        jLabel4.setText("State Least Visited:");
+
+        stateList.setModel(new javax.swing.DefaultComboBoxModel(new String[] { "Select State", "New York", "New Jersy", "Massachusets", "Washingtin DC" }));
+
+        jLabel5.setText("IT Fields of Interests (Check all Apply)");
+
+        securityCB.setText("Security");
+        securityCB.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                securityCBActionPerformed(evt);
+            }
+        });
+
+        appCB.setText("Apps");
+
+        roboticCB.setText("Robotic");
+
+        clearB.setText("Clear ");
+        clearB.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                clearBActionPerformed(evt);
+            }
+        });
+
+        submitB.setText("Submit");
+
+        exitB.setText("Exit");
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addGap(27, 27, 27)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
-                    .addComponent(jLabel1)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                            .addComponent(jLabel3)
+                            .addComponent(jLabel4))
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addGroup(layout.createSequentialGroup()
+                                .addComponent(redRB)
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                                .addComponent(greenRB)
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                                .addComponent(blueRB))
+                            .addComponent(stateList, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)))
                     .addGroup(layout.createSequentialGroup()
-                        .addComponent(jLabel2)
+                        .addGap(79, 79, 79)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(jLabel5)
+                            .addGroup(layout.createSequentialGroup()
+                                .addComponent(jLabel2)
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                                .addComponent(nameTextField, javax.swing.GroupLayout.PREFERRED_SIZE, 156, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                        .addGap(0, 0, Short.MAX_VALUE)))
+                .addGap(109, 109, 109))
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(securityCB)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(appCB)
+                .addGap(18, 18, 18)
+                .addComponent(roboticCB)
+                .addGap(110, 110, 110))
+            .addGroup(layout.createSequentialGroup()
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(135, 135, 135)
+                        .addComponent(jLabel1))
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(64, 64, 64)
+                        .addComponent(clearB)
                         .addGap(18, 18, 18)
-                        .addComponent(nameTextField, javax.swing.GroupLayout.PREFERRED_SIZE, 156, javax.swing.GroupLayout.PREFERRED_SIZE))
-                    .addGroup(layout.createSequentialGroup()
-                        .addComponent(jLabel3)
-                        .addGap(174, 174, 174)))
-                .addContainerGap(101, Short.MAX_VALUE))
+                        .addComponent(submitB)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                        .addComponent(exitB)))
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addGap(28, 28, 28)
+                .addGap(22, 22, 22)
                 .addComponent(jLabel1)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addGap(18, 18, 18)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(jLabel2)
                     .addComponent(nameTextField, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addGap(18, 18, 18)
-                .addComponent(jLabel3)
-                .addContainerGap(182, Short.MAX_VALUE))
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabel3)
+                    .addComponent(redRB)
+                    .addComponent(greenRB)
+                    .addComponent(blueRB))
+                .addGap(18, 18, 18)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabel4)
+                    .addComponent(stateList, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addComponent(jLabel5)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(securityCB)
+                    .addComponent(appCB)
+                    .addComponent(roboticCB))
+                .addGap(18, 18, 18)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(submitB)
+                    .addComponent(clearB)
+                    .addComponent(exitB))
+                .addContainerGap(20, Short.MAX_VALUE))
         );
 
         pack();
@@ -83,6 +187,14 @@ public class Hwk2JFrame extends javax.swing.JFrame {
     private void nameTextFieldActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_nameTextFieldActionPerformed
         // TODO add your handling code here:
     }//GEN-LAST:event_nameTextFieldActionPerformed
+
+    private void securityCBActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_securityCBActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_securityCBActionPerformed
+
+    private void clearBActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_clearBActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_clearBActionPerformed
 
     /**
      * @param args the command line arguments
@@ -120,9 +232,22 @@ public class Hwk2JFrame extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JCheckBox appCB;
+    private javax.swing.JRadioButton blueRB;
+    private javax.swing.JButton clearB;
+    private javax.swing.ButtonGroup colorGroup;
+    private javax.swing.JButton exitB;
+    private javax.swing.JRadioButton greenRB;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
+    private javax.swing.JLabel jLabel4;
+    private javax.swing.JLabel jLabel5;
     private javax.swing.JTextField nameTextField;
+    private javax.swing.JRadioButton redRB;
+    private javax.swing.JCheckBox roboticCB;
+    private javax.swing.JCheckBox securityCB;
+    private javax.swing.JComboBox stateList;
+    private javax.swing.JButton submitB;
     // End of variables declaration//GEN-END:variables
 }
