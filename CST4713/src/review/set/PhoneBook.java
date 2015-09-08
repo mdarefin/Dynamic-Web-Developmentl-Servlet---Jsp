d86de4a9faeb29c94e1cc45463dcc2f4a24118a2
Md Arefin <arefin.shahrear@gmail.com>
2015-09-08 08:53:04 -0400

    adding setter

--- CST4713/src/review/set/PhoneBook.java
+++ CST4713/src/review/set/PhoneBook.java
@@ -2,19 +2,20 @@
 package review.set;
 
 public class PhoneBook {
-    
+
     private String name, phoneNumber;
-    
+
     public PhoneBook(String name, String phoneNumber) {
         this.name = name;
         this.phoneNumber = phoneNumber;
     }
-    
-    public PhoneBook(PhoneBook entry){
+
+    public PhoneBook(PhoneBook entry) {
         this.name = entry.getName();
         this.phoneNumber = entry.getPhoneNumber();
     }
 
+    //getting the name and number
     private String getName() {
         return name;
     }
@@ -22,4 +23,14 @@ public class PhoneBook {
     private String getPhoneNumber() {
         return phoneNumber;
     }
+
+    //setting the phone name and number 
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
 }
