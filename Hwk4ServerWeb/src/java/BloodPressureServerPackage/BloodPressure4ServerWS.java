925aacbf738e906882db6f7efb480e19099e9548
Md Arefin <arefin.shahrear@gmail.com>
2015-09-17 08:35:33 -0400

    adding new parameter

--- Hwk4ServerWeb/src/java/BloodPressureServerPackage/BloodPressure4ServerWS.java
+++ Hwk4ServerWeb/src/java/BloodPressureServerPackage/BloodPressure4ServerWS.java
@@ -1,26 +1,20 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+//Hwk4: given blood pressure (duastolic and systolic) service return medical status
 package BloodPressureServerPackage;
 
 import javax.jws.WebService;
 import javax.jws.WebMethod;
 import javax.jws.WebParam;
 
-/**
- *
- * @author WaLiD
- */
+
 @WebService(serviceName = "BloodPressure4ServerWS")
 public class BloodPressure4ServerWS {
 
     /**
-     * This is a sample web service operation
+     * Web service operation
      */
-    @WebMethod(operationName = "hello")
-    public String hello(@WebParam(name = "name") String txt) {
-        return "Hello " + txt + " !";
+    @WebMethod(operationName = "BloodPressureServerOp")
+    public String BloodPressureServerOp(@WebParam(name = "inputps") double inputps, @WebParam(name = "inputpd") double inputpd) {
+        //TODO write your implementation code here:
+        return null;
     }
 }
