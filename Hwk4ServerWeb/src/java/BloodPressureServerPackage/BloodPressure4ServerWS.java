898dcedd1ce86db613e255b437e483c6c21aa415
Md Arefin <arefin.shahrear@gmail.com>
2015-09-17 09:03:05 -0400

    fix formatting

--- Hwk4ServerWeb/src/java/BloodPressureServerPackage/BloodPressure4ServerWS.java
+++ Hwk4ServerWeb/src/java/BloodPressureServerPackage/BloodPressure4ServerWS.java
@@ -13,40 +13,39 @@ public class BloodPressure4ServerWS {
      */
     @WebMethod(operationName = "BloodPressureServerOp")
     public String BloodPressureServerOp(@WebParam(name = "inputps") double inputps, @WebParam(name = "inputpd") double inputpd) {
-        
+
         //crete string variable to hold BllodPressure 
         String pressureStatus = "";
-        
+
         //Check the pressure range input fall into low blood pressure 
-        if(inputps >= 70 && inputps < 90){
-            if(inputpd >=40 && inputpd < 60){
+        if (inputps >= 70 && inputps < 90) {
+            if (inputpd >= 40 && inputpd < 60) {
                 pressureStatus = "low blood pressure";
             }
         }
-        
+
         //Check the pressure range input fall into Ideal Blood Pressure 
-        if(inputps >= 90 && inputps < 120){
-            if(inputpd >=60 && inputpd < 80){
+        if (inputps >= 90 && inputps < 120) {
+            if (inputpd >= 60 && inputpd < 80) {
                 pressureStatus = "Ideal Blood Pressure";
             }
         }
-        
-         //Check the pressure range input fall into Pre-High Blood Pressure
-        if(inputps >= 120 && inputps < 140){
-            if(inputpd >=80 && inputpd < 90){
+
+        //Check the pressure range input fall into Pre-High Blood Pressure
+        if (inputps >= 120 && inputps < 140) {
+            if (inputpd >= 80 && inputpd < 90) {
                 pressureStatus = "Pre-High Blood Pressure";
             }
         }
-        
-         //Check the pressure range input fall into High Blood Pressure
-        if(inputps > 140 && inputpd > 90){
-            
-                pressureStatus = "Pre-High Blood Pressure";
-            }
 
-        
-        return "Your systolic pressure of "+ inputps+
-                " and your diastolic pressure of "+inputpd+
-                " Your Boold Pressure Status: "+pressureStatus;
+        //Check the pressure range input fall into High Blood Pressure
+        if (inputps > 140 && inputpd > 90) {
+
+            pressureStatus = "Pre-High Blood Pressure";
+        }
+
+        return "Your systolic pressure of " + inputps
+                + " and your diastolic pressure of " + inputpd
+                + " Your Boold Pressure Status: " + pressureStatus;
     }
 }
