2a130d7cd43e9af73806417bb8655a540afccd81
Md Arefin <arefin.shahrear@gmail.com>
2015-10-01 14:27:25 -0400

    add an algorithm to get the translated word

--- WordTranslatorTest1Server/src/java/WordTranslatorPackage/WordTranslatorWS.java
+++ WordTranslatorTest1Server/src/java/WordTranslatorPackage/WordTranslatorWS.java
@@ -1,6 +1,8 @@
 //
 package WordTranslatorPackage;
 
+import java.util.ArrayList;
+import java.util.List;
 import javax.jws.WebService;
 import javax.jws.WebMethod;
 import javax.jws.WebParam;
@@ -12,59 +14,41 @@ public class WordTranslatorWS {
      * Web service operation
      */
     @WebMethod(operationName = "languaeOperation")
-    public String languaeOperation(@WebParam(name = "inputWordEng") String inputWordEng, @WebParam(name = "inputWordTranslet") String inputWordTranslet) {
+    public String languaeOperation(@WebParam(name = "inputWord") String inputWord, @WebParam(name = "inputLanguage") String inputLanguage) {
 
         //we get word and we will translate the word to other language
-        String translatedWord = "";
+        String translation = "";
 
-        if (inputWordEng.equalsIgnoreCase("Thank You") || inputWordEng.equalsIgnoreCase("Good morning")) {
+        List<String[]> languageList = new ArrayList();
+        String[] Arabic = {"Arabic", "Shukran", "saba il khayr"};
+        String[] Chinese = {"Chinese", "xiexie", "zaoan"};
+        String[] French = {"French", "merci", "bonjour"};
+        String[] German = {"German", "danke", "guten morgen"};
+        String[] Hindi = {"Hindi", "dhanyavad", "namaste"};
+        String[] Japanese = {"Japanese", "domo", "ohayo gozaimasu"};
+        String[] Spanish = {"Spanish", "gracias", "buenos dias"};
+        String[] Portuguese = {"Portuguese", "obrigado", "bom dia"};
 
-            if (inputWordTranslet.equalsIgnoreCase("Arabic") && inputWordEng.equalsIgnoreCase("Thank You")) {
-                translatedWord = "shukran";
-            } else if (inputWordTranslet.equalsIgnoreCase("Arabic") && inputWordEng.equalsIgnoreCase("Good morning")) {
-                translatedWord = "saba il khayr";
-            }
-            if (inputWordTranslet.equalsIgnoreCase("Chinese") && inputWordEng.equalsIgnoreCase("Thank You")) {
-                translatedWord = "xiexie";
-            } else if (inputWordTranslet.equalsIgnoreCase("Chinese") && inputWordEng.equalsIgnoreCase("Good morning")) {
-                translatedWord = "zaoan";
-            }
-            if (inputWordTranslet.equalsIgnoreCase("French") && inputWordEng.equalsIgnoreCase("Thank You")) {
-                translatedWord = "merci";
-            } else if (inputWordTranslet.equalsIgnoreCase("French") && inputWordEng.equalsIgnoreCase("Good morning")) {
-                translatedWord = "bonjour";
-            }
-            if (inputWordTranslet.equalsIgnoreCase("German") && inputWordEng.equalsIgnoreCase("Thank You")) {
-                translatedWord = "danke";
-            } else if (inputWordTranslet.equalsIgnoreCase("German") && inputWordEng.equalsIgnoreCase("Good morning")) {
-                translatedWord = "guten morgen";
-            }
-            if (inputWordTranslet.equalsIgnoreCase("Hindi") && inputWordEng.equalsIgnoreCase("Thank You")) {
-                translatedWord = "dhanyavad";
-            } else if (inputWordTranslet.equalsIgnoreCase("Hindi") && inputWordEng.equalsIgnoreCase("Good morning")) {
-                translatedWord = "namaste";
-            }
-            if (inputWordTranslet.equalsIgnoreCase("Japanese") && inputWordEng.equalsIgnoreCase("Thank You")) {
-                translatedWord = "domo";
-            } else if (inputWordTranslet.equalsIgnoreCase("Japanese") && inputWordEng.equalsIgnoreCase("Good morning")) {
-                translatedWord = "ohayo gozaimasu";
-            }
-            if (inputWordTranslet.equalsIgnoreCase("Spanish") && inputWordEng.equalsIgnoreCase("Thank You")) {
-                translatedWord = "gracias";
-            } else if (inputWordTranslet.equalsIgnoreCase("Spanish") && inputWordEng.equalsIgnoreCase("Good morning")) {
-                translatedWord = "buenos dias";
-            }
-            if (inputWordTranslet.equalsIgnoreCase("Portuguese") && inputWordEng.equalsIgnoreCase("Thank You")) {
-                translatedWord = "obrigado";
-            } else if (inputWordTranslet.equalsIgnoreCase("Portuguese") && inputWordEng.equalsIgnoreCase("Good morning")) {
-                translatedWord = "bom dia";
-            }
+        languageList.add(Arabic);
+        languageList.add(Chinese);
+        languageList.add(French);
 
-        } else {
-            translatedWord = "Please Enter Thank You or Good Morning to see their Tranlation";
+        for (String[] langue : languageList) {
+            for (String langue1 : langue) {
+                if (langue1.equalsIgnoreCase(inputLanguage)) {
+                    if (inputWord.equalsIgnoreCase("Thank you ")) {
+                        translation = langue[1];
+                    } else if (inputWord.equalsIgnoreCase("Good Morning")) {
+                        translation = langue[2];
+                    }
+
+                }
+
+            }
         }
 
-        return translatedWord;
+        return translation;
+
     }
 
 }
