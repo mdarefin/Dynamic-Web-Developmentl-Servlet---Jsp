a342977a03025850f5f8a39cd238f684a7f5f6a9
Md Arefin <arefin.shahrear@gmail.com>
2015-10-06 08:24:43 -0400

    adding audio files

--- MovieSelectionServer/src/java/MovieWebServerPackage/MovieWebServerWS.java
+++ MovieSelectionServer/src/java/MovieWebServerPackage/MovieWebServerWS.java
@@ -1,35 +1,45 @@
 //
-
 package MovieWebServerPackage;
 
+import java.io.FileInputStream;
 import java.util.ArrayList;
 import java.util.List;
 import javax.jws.WebService;
 import javax.jws.WebMethod;
 import javax.jws.WebParam;
+import java.io.FileInputStream;
 
-
-@WebService(serviceName="MovieWebServerWS")
+@WebService(serviceName = "MovieWebServerWS")
 public class MovieWebServerWS {
 
     /**
      * Web service operation
      */
     @WebMethod(operationName = "operation")
-    public String operation(@WebParam(name = "inputSound") String inputSound) {
+    public void operation(@WebParam(name = "inputSound") String inputSound) {
         //getting the option choice 
-        
+
+        try{
+
+             FileInputStream fis = new FileInputStream("File location.");
+             //Player playMP3 = new Player(fis);
+
+             //playMP3.play();
+
+        }  catch(Exception e){
+             System.out.println(e);
+           }
+
         List<String> soundList = new ArrayList<>();
         soundList.add("Lion Roaring");
         soundList.add("Tiger Roaring");
         soundList.add("Elephant Mother");
-        
-        if(soundList.contains(inputSound)){
-          return inputSound;
-        }
-        
-        return "return a valid value";
-    }
 
+        if (soundList.contains(inputSound)) {
+            // inputSound;
+           // mediaPlayer.play();
+        }
+
+    }
 
 }
